c97ff81464ad1e4be8320a7d8b2d5de3be6b01bb
hidayahhtaufikk <hidayahhtaufik12@gmail.com>
2021-06-29 20:00:31 +0700

    feat(admin): fetch profiles via swr

--- apps/qopnet-admin/src/app/pages/profiles.tsx
+++ apps/qopnet-admin/src/app/pages/profiles.tsx
@@ -1,7 +1,24 @@
+import useSWR from 'swr'
+
 export const Profiles = () => {
+  const fetcher = (url: string) =>
+    fetch(process.env.NX_API_URL + url).then((res) => res.json())
+
+  const { data, error } = useSWR('/api/profiles', fetcher)
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+
   return (
     <div>
       <h1>Profiles</h1>
+      {data.map((profile: any) => {
+        return (
+          <div>
+            <h1>{profile.name}</h1>
+          </div>
+        )
+      })}
     </div>
   )
 }
