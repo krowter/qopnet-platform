5080d02c58dab91ade87e225f18d92f3e5e06b97
M Haidar Hanif <git@mhaidarhanif.com>
2021-07-07 20:16:58 +0700

    feat(api): signup, signin, signout via supabase

--- apps/qopnet-api/src/app/auth/index.ts
+++ apps/qopnet-api/src/app/auth/index.ts
@@ -1,12 +1,32 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { supabase } from '@qopnet/util-supabase'
 
 import * as express from 'express'
 const router = express.Router()
 
-router.get('/', async (req, res) => {
-  const profiles = await prisma.profile.findMany({})
-  res.json(profiles)
+router.post('/signup', async (req, res) => {
+  const { user, error } = await supabase.auth.signUp({
+    email: req.body.email,
+    password: req.body.password,
+  })
+  if (error) res.status(500).json({ error })
+  else res.json({ user })
+})
+
+router.post('/signin', async (req, res) => {
+  const { session, error } = await supabase.auth.signIn({
+    email: req.body.email,
+    password: req.body.password,
+  })
+
+  if (error) res.status(500).json({ error })
+  else res.json({ session })
+})
+
+router.post('/signout', async (req, res) => {
+  const { error } = await supabase.auth.signOut()
+
+  if (error) res.status(500).json({ error })
+  else res.json({ message: 'User signed out' })
 })
 
 export default router
