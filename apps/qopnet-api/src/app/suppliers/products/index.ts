ad1e0ec9c3efb5611e7a6dad459b69ebadee8dd2
windo <herwindo.artono@gmail.com>
2021-07-10 14:57:59 +0700

    feat(api): create supplierProducts api endpoints
    Get supplierProduct by product slug
    Create supplierProduct

--- apps/qopnet-api/src/app/suppliers/products/index.ts
+++ apps/qopnet-api/src/app/suppliers/products/index.ts
@@ -1,6 +1,5 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, Prisma } from '@prisma/client'
 const prisma = new PrismaClient()
-
 import * as express from 'express'
 const router = express.Router()
 
@@ -10,6 +9,41 @@ router.get('/', async (req, res) => {
     message: 'Get all supplier products',
     supplierProducts,
   })
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const supplierProduct: Prisma.SupplierProductCreateInput = req.body.supplierProduct
+    supplierProduct.createdAt = new Date()
+    supplierProduct.updatedAt = new Date()
+
+    const createSupplierProduct = await prisma.supplierProduct.create({
+      data: supplierProduct,
+    })
+
+    res.json({
+      message: 'Create Supplier Product',
+      supplierProduct: createSupplierProduct,
+    })
+  } catch (e) {
+    res.json(e)
+    return next(e)
+  }
+})
+
+router.get('/:slug', async (req, res, next) => {
+  try {
+    const { slug } = req.params
+    const supplierProduct = await prisma.supplierProduct.findUnique({
+      where: { slug },
+    })
+    res.json({
+      message: 'Get supplier product by product slug',
+      supplierProduct,
+    })
+  } catch (e) {
+    next(e)
+  }
 })
 
 export default router
