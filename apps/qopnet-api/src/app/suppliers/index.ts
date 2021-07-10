483ddbf7ab5f765ec1f48cd670a42f1a8dc754ab
windo <herwindo.artono@gmail.com>
2021-07-10 18:51:09 +0700

    feat(api): get all supplier products by supplier handle

--- apps/qopnet-api/src/app/suppliers/index.ts
+++ apps/qopnet-api/src/app/suppliers/index.ts
@@ -9,6 +9,10 @@ router.get('/:id/products', async (req, res, next) => {
     const supplierProduct = await prisma.supplier.findUnique({
       where: { id },
     })
+    // if not found try to find it by product handle
+    if (!supplierProduct) {
+        return next()
+    }
     res.json({
       message: 'Get all supplier products by supplierId',
       supplierProduct,
@@ -17,5 +21,21 @@ router.get('/:id/products', async (req, res, next) => {
     next(e)
   }
 })
+
+router.get('/:handle/products', async (req, res, next) => {
+    try {
+      const { handle } = req.params
+      console.log(handle)
+      const supplierProduct = await prisma.supplier.findUnique({
+        where: { handle },
+      })
+      res.json({
+        message: 'Get all supplier products by supplier handle',
+        supplierProduct,
+      })
+    } catch (e) {
+      next(e)
+    }
+  })
 
 export default router
