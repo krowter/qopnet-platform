9b090a362436b228940dfc40647311c8c97d890a
M Haidar Hanif <git@mhaidarhanif.com>
2021-07-12 16:14:41 +0700

    feat(api): get my profile via jwt

--- apps/qopnet-api/src/app/profiles/index.ts
+++ apps/qopnet-api/src/app/profiles/index.ts
@@ -4,9 +4,25 @@ const prisma = new PrismaClient()
 import * as express from 'express'
 const router = express.Router()
 
+import { checkUser } from '../auth/middleware'
+
 router.get('/', async (req, res) => {
   const profiles = await prisma.profile.findMany({})
-  res.json(profiles)
+  res.json({
+    message: 'Get all profiles',
+    profiles,
+  })
+})
+
+router.get('/my', checkUser, async (req, res) => {
+  const profile = await prisma.profile.findFirst({
+    where: { userId: req.user.sub },
+  })
+
+  res.json({
+    message: 'Get my profile',
+    profile,
+  })
 })
 
 export default router
