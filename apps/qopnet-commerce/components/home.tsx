0b3ab679ab725079d9a99c139cfe74a70b257791
M Haidar Hanif <git@mhaidarhanif.com>
2021-07-14 02:34:40 +0700

    feat(commerce): ui home banner with random images

--- apps/qopnet-commerce/components/home.tsx
+++ apps/qopnet-commerce/components/home.tsx
@@ -1,9 +1,11 @@
 import { Stack, Box } from '@chakra-ui/react'
 
+import { HomeBanner } from '@qopnet/qopnet-ui'
+
 export const Home = () => {
   return (
     <Stack>
-      <Box id="slider">Promo terkini</Box>
+      <HomeBanner id="slider-promo" />
       <Box id="product-category">Kategori produk</Box>
       <Box id="product-special">Produk pilihan</Box>
       <Box id="faq">Pertanyaan yang sering ditanya</Box>
---
+++ libs/qopnet-ui/src/lib/home-banner/home-banner.spec.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react'
+
+import HomeBanner from './home-banner'
+
+describe('HomeBanner', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<HomeBanner />)
+    expect(baseElement).toBeTruthy()
+  })
+})
