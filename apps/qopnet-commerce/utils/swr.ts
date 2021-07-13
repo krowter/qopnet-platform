7ddef24f07d6c297c617e02284b22a785a87a90e
M Haidar Hanif <git@mhaidarhanif.com>
2021-07-14 06:35:24 +0700

    fix(commerce): dynamic fallback api url

--- apps/qopnet-commerce/utils/swr.ts
+++ apps/qopnet-commerce/utils/swr.ts
@@ -1,24 +1,33 @@
+/**
+ * This swr utility is to configure @qopnet/util-swr
+ * for specific Qopnet Commerce app because the lib cannot access process.env
+ */
 import {
   useSWR as useSWROriginal,
   fetcher as utilFetcher,
 } from '@qopnet/util-swr'
 
-// Check apiUrl in Qopnet Commerce
-// On server side only
-if (process.env.NODE_ENV === 'production' && typeof window === 'undefined') {
-  console.info({
-    apiUrl: process.env.NEXT_PUBLIC_NX_API_URL,
-    env: process.env,
-  })
+/**
+ * Set apiUrl based on variable availability
+ * There is still an issue on Vercel deployment
+ */
+
+const apiUrl =
+  process.env.NEXT_PUBLIC_NX_API_URL ||
+  process.env.NEXT_PUBLIC_API_URL ||
+  'https://qopnet-api.catamyst.com'
+console.info({ apiUrl })
+
+/**
+ * Dynamic fetcher which use apiUrl automatically
+ */
+export const fetcher = async (endpoint: string) => {
+  return await utilFetcher(apiUrl, endpoint)
 }
 
+/**
+ * Wrap original useSWR so we don't need to import fetcher manually each time
+ */
 export const useSWR = (endpoint: string) => {
   return useSWROriginal(endpoint, fetcher)
-}
-
-export const fetcher = async (endpoint: string) => {
-  return await utilFetcher(
-    process.env.NEXT_PUBLIC_NX_API_URL || process.env.NEXT_PUBLIC_API_URL,
-    endpoint
-  )
 }
