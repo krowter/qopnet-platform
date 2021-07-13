a2fdffbdd1f21dbde5579e5e530b1112c55fb43f
windo <herwindo.artono@gmail.com>
2021-07-13 12:41:55 +0700

    feat(api): create supplier product w supplierParam

--- apps/qopnet-api/src/app/suppliers/index.ts
+++ apps/qopnet-api/src/app/suppliers/index.ts
@@ -1,7 +1,9 @@
-import { PrismaClient, Prisma } from '@prisma/client'
+import { PrismaClient, Prisma, SupplierProduct } from '@prisma/client'
 const prisma = new PrismaClient()
 import * as express from 'express'
 const router = express.Router()
+import slugify from 'slugify'
+import { checkUser } from '../auth/middleware'
 
 router.get('/:id/products', async (req, res, next) => {
   try {
@@ -11,7 +13,7 @@ router.get('/:id/products', async (req, res, next) => {
     })
     // if not found try to find it by product handle
     if (!supplierProduct) {
-        return next()
+      return next()
     }
     res.json({
       message: 'Get all supplier products by supplierId',
@@ -23,19 +25,94 @@ router.get('/:id/products', async (req, res, next) => {
 })
 
 router.get('/:handle/products', async (req, res, next) => {
-    try {
-      const { handle } = req.params
-      console.log(handle)
-      const supplierProduct = await prisma.supplier.findUnique({
-        where: { handle },
-      })
+  try {
+    const { handle } = req.params
+    console.log(handle)
+    const supplierProduct = await prisma.supplier.findUnique({
+      where: { handle },
+    })
+    res.json({
+      message: 'Get all supplier products by supplier handle',
+      supplierProduct,
+    })
+  } catch (e) {
+    next(e)
+  }
+})
+
+router.post('/:supplierParam/products', checkUser, async (req, res, next) => {
+  const userId = req.user.sub!
+  let supplierId = ''
+  const { supplierParam } = req.params
+
+  // check if user exist by userId
+  try {
+    const user = await prisma.user.findUnique({ where: { id: userId } })
+    if (!user) throw new Error('User not found')
+  } catch (error) {
+    res.json({
+      message: error.message,
+    })
+    return next(error)
+  }
+
+  // get supplier id
+  try {
+    const supplier = await prisma.supplier.findUnique({
+      where: {
+        handle: supplierParam,
+      },
+    })
+    if (!supplier) throw new Error('Supplier not found')
+    supplierId = supplier.id
+  } catch (error) {
+    res.json({
+      message: error.message,
+      supplierParam,
+      error,
+    })
+    return next(error)
+  }
+
+  const supplierProduct: Prisma.SupplierProductCreateInput =
+    req.body.supplierProduct
+  const supplierProductSlug = slugify(supplierProduct.name.toLowerCase())
+
+  try {
+    const newSupplierProduct: SupplierProduct = await prisma.supplierProduct.create(
+      {
+        data: {
+          ...supplierProduct,
+          ownerId: userId,
+          supplierId: supplierId,
+          slug: supplierProductSlug,
+        },
+      }
+    )
+
+    res.json({
+      message: 'Create new supplier product',
+      supplierProduct: newSupplierProduct,
+    })
+  } catch (error) {
+    if (error.code === 'P2002') {
       res.json({
-        message: 'Get all supplier products by supplier handle',
-        supplierProduct,
+        message: `Failed to create unique slug`,
+        slug: supplierProductSlug,
+        error,
       })
-    } catch (e) {
-      next(e)
+      return next(error)
+    } else if (error.code === 'P2011') {
+      res.json({
+        message: `Field cannot be empty`,
+        field: error.meta.constraint,
+        error,
+      })
+    } else {
+      res.json(error)
+      return next(error)
     }
-  })
+  }
+})
 
 export default router
--- apps/qopnet-api/src/app/suppliers/products/index.ts
+++ apps/qopnet-api/src/app/suppliers/products/index.ts
@@ -1,7 +1,4 @@
-import slugify from 'slugify'
-import { checkUser } from '../../auth/middleware'
-
-import { PrismaClient, Prisma, SupplierProduct } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 import * as express from 'express'
@@ -14,57 +11,6 @@ router.get('/', async (req, res) => {
     message: 'Get all supplier products',
     supplierProducts,
   })
-})
-
-router.post('/', checkUser, async (req, res, next) => {
-  const userId = req.user.sub!
-
-  // check if user exist by userId
-  try {
-    const user = await prisma.user.findUnique({ where: { id: userId } })
-    if (!user) throw new Error('User not found')
-  } catch (error) {
-    res.json({
-      message: error.message,
-    })
-    return next(error)
-  }
-  const supplierProduct: Prisma.SupplierProductCreateInput =
-    req.body.supplierProduct
-  const supplierProductSlug = slugify(supplierProduct.name.toLowerCase())
-
-  try {
-    const newSupplierProduct: SupplierProduct = await prisma.supplierProduct.create({
-      data: {
-        ...supplierProduct,
-        ownerId: userId,
-        slug: supplierProductSlug,
-      },
-    })
-
-    res.json({
-      message: 'Create new supplier product',
-      supplierProduct: newSupplierProduct,
-    })
-  } catch (error) {
-    if (error.code === 'P2002') {
-      res.json({
-        message: `Failed to create unique slug`,
-        slug: supplierProductSlug,
-        error,
-      })
-      return next(error)
-    } else if (error.code === 'P2011') {
-      res.json({
-        message: `Field cannot be empty`,
-        field: error.meta.constraint,
-        error,
-      })
-    } else {
-      res.json(error)
-      return next(error)
-    }
-  }
 })
 
 router.get('/:slug', async (req, res, next) => {
