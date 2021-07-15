b5c0df3093f63f22c1e9524ca03e4ca4f3b5f9d2
M Haidar Hanif <git@mhaidarhanif.com>
2021-07-16 05:40:54 +0700

    feat(commerce): search bar actual results via swr

--- apps/qopnet-commerce/pages/search/index.tsx
+++ apps/qopnet-commerce/pages/search/index.tsx
@@ -1,7 +1,15 @@
 import { useRouter } from 'next/router'
-import { Stack, Heading } from '@chakra-ui/react'
+import {
+  Stack,
+  Heading,
+  Text,
+  Flex,
+  Spinner,
+  SimpleGrid,
+} from '@chakra-ui/react'
 
-import { Layout } from '@qopnet/qopnet-ui'
+import { Layout, SupplierProductCard } from '@qopnet/qopnet-ui'
+import { useSWR } from '../../utils/swr'
 
 const SearchPage = () => {
   const router = useRouter()
@@ -29,6 +37,9 @@ export const SearchBegin = () => {
 }
 
 export const SearchResults = ({ keyword }) => {
+  const { data, error } = useSWR(`/api/suppliers/products/search?q=${keyword}`)
+  const { count, supplierProducts } = data || {}
+
   return (
     <Stack pt={10} spacing={5}>
       <Heading as="h1" size="xl">
@@ -37,6 +48,25 @@ export const SearchResults = ({ keyword }) => {
       <Heading as="h2" size="md">
         Hasil pencarian untuk <b>"{keyword}"</b>
       </Heading>
+      {error && <Text>Gagal mencari produk dan supplier</Text>}
+      {!error && !supplierProducts && (
+        <Flex>
+          <Spinner mr={5} />
+          <Text>Mencari produk dan supplier...</Text>
+        </Flex>
+      )}
+      {!error && supplierProducts && (
+        <SimpleGrid spacing={5} columns={[1, 2, 4]}>
+          {supplierProducts?.map((product, index) => {
+            return (
+              <SupplierProductCard
+                key={product.slug || index}
+                product={product}
+              />
+            )
+          })}
+        </SimpleGrid>
+      )}
     </Stack>
   )
 }
