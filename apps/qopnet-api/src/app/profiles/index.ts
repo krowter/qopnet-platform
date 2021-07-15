2f012f86ac3122d4cfd5b7c15566661bb48c37f8
M Haidar Hanif <git@mhaidarhanif.com>
2021-07-15 07:14:19 +0700

    feat(api): get all profiles include user email

--- apps/qopnet-api/src/app/profiles/index.ts
+++ apps/qopnet-api/src/app/profiles/index.ts
@@ -7,20 +7,37 @@ const router = express.Router()
 
 import { checkUser } from '../auth/middleware'
 
+/**
+ * GET /api/profiles
+ */
 router.get('/', async (req, res) => {
-  const profiles = await prisma.profile.findMany({})
-  res.json({
-    message: 'Get all profiles',
-    profiles,
-  })
+  try {
+    const profiles = await prisma.profile.findMany({
+      include: { user: true },
+    })
+    res.json({
+      message: 'Get all profiles success',
+      profiles,
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: 'Get all profiles failed',
+      error,
+    })
+  }
 })
 
+/**
+ * POST /api/profiles
+ */
 router.post('/', checkUser, async (req, res, next) => {
   const userId = req.body.userId
 
-  // check if user exist by userId
   try {
-    const user = await prisma.user.findUnique({ where: { id: userId } })
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      include: { profile: true },
+    })
     if (!user) throw new Error('User not found')
   } catch (error) {
     res.json({
@@ -30,19 +47,19 @@ router.post('/', checkUser, async (req, res, next) => {
   }
 
   try {
-    const profile = await prisma.profile.findUnique({ where: { handle: req.body.handle } })
+    const profile = await prisma.profile.findUnique({
+      where: { handle: req.body.handle },
+    })
     if (!profile) {
-      const newProfile = await prisma.profile.create(
-        {
-          data: {
-            name: req.body.name,
-            avatarUrl: req.body.avatarUrl,
-            phone: req.body.phone,
-            userId: req.body.userId,
-            handle: req.body.handle
-          },
-        }
-      )
+      const newProfile = await prisma.profile.create({
+        data: {
+          name: req.body.name,
+          avatarUrl: req.body.avatarUrl,
+          phone: req.body.phone,
+          userId: req.body.userId,
+          handle: req.body.handle,
+        },
+      })
 
       res.json({
         message: 'Create new user profile',
