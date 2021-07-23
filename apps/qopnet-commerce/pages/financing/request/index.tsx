1e8aa7d012e44f63ca868dcd814eb449107252b7
M Haidar Hanif <git@mhaidarhanif.com>
2021-07-24 01:46:52 +0700

    feat(commerce): dashboard link to financing request
    and other layout improvement

---
+++ apps/qopnet-commerce/pages/financing/request/index.tsx
@@ -0,0 +1,16 @@
+import { Layout } from '@qopnet/qopnet-ui'
+
+export const FinancingRequestPage = () => {
+  return (
+    <Layout
+      meta={{
+        title: 'Ajukan pinjaman',
+        description: 'Daftar untuk mengajukan pinjaman',
+      }}
+    >
+      <h1>Daftar untuk mengajukan pinjaman</h1>
+    </Layout>
+  )
+}
+
+export default FinancingRequestPage
