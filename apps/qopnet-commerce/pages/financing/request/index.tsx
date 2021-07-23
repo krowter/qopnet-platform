4533cd26158cc11c785b887cf55f28161603dc47
M Haidar Hanif <git@mhaidarhanif.com>
2021-07-24 01:53:27 +0700

    feat(commerce): financing page with request fund form

--- apps/qopnet-commerce/pages/financing/request/index.tsx
+++ apps/qopnet-commerce/pages/financing/request/index.tsx
@@ -1,15 +1,51 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { useUser } from 'use-supabase'
+import { Text, Stack, Heading, HStack, Spinner } from '@chakra-ui/react'
+
 import { Layout } from '@qopnet/qopnet-ui'
+import { useSWR } from '../../../utils/swr'
 
 export const FinancingRequestPage = () => {
+  const user = useUser()
+  const router = useRouter()
+  useEffect(() => {
+    if (!user) {
+      router.push('/signin')
+    }
+  }, [user, router])
+
+  const { data, error } = useSWR('/api/profiles/my')
+  const { profile } = data || {}
+
   return (
     <Layout
+      pt={10}
       meta={{
         title: 'Ajukan pinjaman',
         description: 'Daftar untuk mengajukan pinjaman',
       }}
     >
-      <h1>Daftar untuk mengajukan pinjaman</h1>
+      {error && <Text>Gagal memuat profil</Text>}
+      {!error && !data && (
+        <HStack>
+          <Spinner />
+          <Text>Memuat profil dan info pengajuan...</Text>
+        </HStack>
+      )}
+      {!error && data && user && <RequestFundForm profile={profile} />}
     </Layout>
+  )
+}
+
+export const RequestFundForm = ({ profile }) => {
+  return (
+    <Stack>
+      <Heading>Daftar untuk mengajukan pinjaman</Heading>
+      <Stack>
+        <Text>Anda akan mengajukan dengan informasi berikut</Text>
+      </Stack>
+    </Stack>
   )
 }
 
