4401d688a6de8ed5819270f54abbebb3bf83826e
M Haidar Hanif <git@mhaidarhanif.com>
2021-07-25 19:16:37 +0700

    refactor(api): paginate functional programming

--- apps/qopnet-api/src/app/root/middleware.ts
+++ apps/qopnet-api/src/app/root/middleware.ts
@@ -1,20 +1,18 @@
 /**
- * Middleware pagination
- * ref: https://docs.commercelayer.io/api/pagination
- * pagination will only run when page query exist
+ * Express middleware for pagination
+ * Reference: https://docs.commercelayer.io/api/pagination
  */
 export function paginate(req, res, next) {
-  let _pageNumber = 1
-  let _pageSize = 20
   const { page } = req.query
 
-  // Only run pagination query exist
+  // Pagination will only run if page query exist
   if (page) {
-    _pageNumber = page.number ? parseInt(page.number) : _pageNumber
-    _pageSize = page.size ? parseInt(page.size) : _pageSize
+    const pageNumber = page.number ? parseInt(page.number) : 1
+    const pageSize = page.size ? parseInt(page.size) : 20
+
     req.page = page
-    req.skip = (_pageNumber - 1) * _pageSize
-    req.take = _pageSize
+    req.skip = (pageNumber - 1) * pageSize
+    req.take = pageSize
   }
   next()
 }
