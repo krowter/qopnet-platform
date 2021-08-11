338fc5e011eb244972e17cb3c2ca10096653b537
M Haidar Hanif <git@mhaidarhanif.com>
2021-08-12 01:40:06 +0700

    feat(api): get all payment methods and records

--- apps/qopnet-api/src/app/payments/records/index.ts
+++ apps/qopnet-api/src/app/payments/records/index.ts
@@ -2,7 +2,16 @@ import * as express from 'express'
 
 import * as root from '../../root/middleware'
 import * as auth from '../../auth/middleware'
+import * as paymentRecord from './middleware'
 
 const router = express.Router()
+
+// GET /api/payments/records
+router.get(
+  '/',
+  auth.checkUser,
+  root.paginate,
+  paymentRecord.getAllPaymentRecords
+)
 
 export default router
