ac32b495a1ee730c9dd4a4c8550025fec4fb978a
windo <herwindo.artono@gmail.com>
2021-08-13 12:08:17 +0700

    refactor(schema): change to  Date for birthDate

---
+++ prisma/migrations/20210813050438_birth_date_to_date/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "promo_employees" ALTER COLUMN "birthDate" SET DATA TYPE DATE;
+
+-- AlterTable
+ALTER TABLE "promo_submissions" ALTER COLUMN "birthDate" SET DATA TYPE DATE;
