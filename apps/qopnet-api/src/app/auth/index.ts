d037c30acf54608f3d7833fb5fdc25cd02d1be5e
Dicky Muhamad R <dickymr.dev@gmail.com>
2021-10-21 00:03:08 +0700

    refactor: use middleware

--- apps/qopnet-api/src/app/auth/index.ts
+++ apps/qopnet-api/src/app/auth/index.ts
@@ -1,73 +1,16 @@
-import { supabase } from '@qopnet/util-supabase'
-import { prisma } from '@qopnet/util-prisma'
-
 import * as express from 'express'
 const router = express.Router()
 
-import { checkUser } from './middleware'
+import { checkUser, signIn, signOut, signUp } from './middleware'
 
-router.post('/signup', async (req, res) => {
-  try {
-    const { user, error } = await supabase.auth.signUp({
-      email: req.body.email,
-      password: req.body.password,
-    })
+// GET /auth/check-user
+router.get('/check-user', checkUser)
 
-    if (error) throw new Error('Sign up error')
-
-    await prisma.user.create({ data: { id: user.id, email: user.email } })
-
-    res.status(200).json({
-      message: 'Sign up success',
-      user,
-    })
-  } catch (error) {
-    res.status(500).json({
-      message: 'Sign up error',
-      error,
-    })
-  }
-})
-
-router.post('/signin', async (req, res) => {
-  const { session, error } = await supabase.auth.signIn({
-    email: req.body.email,
-    password: req.body.password,
-  })
-
-  if (error) {
-    res.status(500).json({
-      message: 'Sign in error',
-      error,
-    })
-  } else {
-    res.json({
-      message: 'Sign in success',
-      session,
-    })
-  }
-})
-
-router.post('/signout', async (req, res) => {
-  const { error } = await supabase.auth.signOut()
-
-  if (error) {
-    res.status(500).json({
-      message: 'Sign out error',
-      error,
-    })
-  } else {
-    res.json({
-      message: 'Sign out success',
-    })
-  }
-})
-
-router.get('/check-user', checkUser, async (req, res) => {
-  res.status(200).json({
-    message: 'Check user success',
-    user: req.user,
-  })
-})
+// POST /auth/signup
+router.post('/signup', signUp)
+// POST /auth/signin
+router.post('/signin', signIn)
+// POST /auth/signout
+router.post('/signout', signOut)
 
 export default router
--- apps/qopnet-api/src/app/auth/middleware.ts
+++ apps/qopnet-api/src/app/auth/middleware.ts
@@ -1,7 +1,8 @@
+import { supabase } from '@qopnet/util-supabase'
 import { prisma } from '@qopnet/util-prisma'
 import * as jwt from 'jsonwebtoken'
 
-export const checkUser = async (req, res, next) => {
+export const checkUser = async (req, res) => {
   /**
    * Headers
    * Authorization: Bearer eyA.B.C
@@ -18,7 +19,6 @@ export const checkUser = async (req, res, next) => {
           message: 'Check user is failed because there is no user in JWT',
         })
       } else {
-        // console.info({ userInJWT })
         try {
           // Get user id, but not checked yet
           req.user = userInJWT // { sub: "a1b2c3-d4e5f6" }
@@ -28,12 +28,16 @@ export const checkUser = async (req, res, next) => {
             where: { id: req.user.sub },
             include: { profile: true },
           })
-          // console.info({ user })
+
           if (!user) throw 'Failed to findFirst user'
 
           // Assign profile once get the user
           req.profile = user.profile
-          next()
+
+          res.status(200).json({
+            message: 'Check user success',
+            user: req.user,
+          })
         } catch (error) {
           res.status(400).json({
             message: 'Check user is failed because profile not found',
@@ -50,6 +54,67 @@ export const checkUser = async (req, res, next) => {
     res.status(500).json({
       message:
         'Check user is failed because request is not authorized, cannot decode JWT, or user not found',
+      error,
+    })
+  }
+}
+
+export const signUp = async (req, res) => {
+  try {
+    const { user, error } = await supabase.auth.signUp({
+      email: req.body.email,
+      password: req.body.password,
+    })
+
+    if (error) throw new Error('Sign up error')
+
+    await prisma.user.create({ data: { id: user.id, email: user.email } })
+
+    res.status(200).json({
+      message: 'Sign up success',
+      user,
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: 'Sign up error',
+      error,
+    })
+  }
+}
+
+export const signIn = async (req, res) => {
+  try {
+    const { session, error } = await supabase.auth.signIn({
+      email: req.body.email,
+      password: req.body.password,
+    })
+
+    if (error) throw new Error('Sign in error')
+
+    res.status(200).json({
+      message: 'Sign in success',
+      session,
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: 'Sign in error',
+      error,
+    })
+  }
+}
+
+export const signOut = async (req, res) => {
+  try {
+    const { error } = await supabase.auth.signOut()
+
+    if (error) throw new Error('Sign out error')
+
+    res.status(200).json({
+      message: 'Sign out success',
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: 'Sign out error',
       error,
     })
   }
