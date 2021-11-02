38c3656a95c6db3580a813cc552d992da79a68d0
Dicky Muhamad R <dickymr.dev@gmail.com>
2021-11-02 20:44:41 +0700

    feat(api): use payments va

--- apps/qopnet-api/src/app/payments/va/index.ts
+++ apps/qopnet-api/src/app/payments/va/index.ts
@@ -3,8 +3,8 @@ import * as virtualAccount from './middleware'
 
 const router = express.Router()
 
-router.post('/va/permata/getbill', virtualAccount.getBill)
-router.post('/va/permata/paybill', virtualAccount.payBill)
-router.post('/va/permata/revbill', virtualAccount.revBill)
+router.post('/permata/getbill', virtualAccount.getBill)
+router.post('/permata/paybill', virtualAccount.payBill)
+router.post('/permata/revbill', virtualAccount.revBill)
 
 export default router
