9956224e1ab21b57e55bbfcb9e329e929a78d16b
Dicky Muhamad R <dickymr.dev@gmail.com>
2021-11-02 20:52:58 +0700

    feat(api): getbill placeholder implementation

--- apps/qopnet-api/src/app/payments/va/middleware.ts
+++ apps/qopnet-api/src/app/payments/va/middleware.ts
@@ -1,12 +1,42 @@
 export const getBill = async (req, res) => {
+  const requestSample = {
+    GetBillRq: {
+      INSTCODE: '7301',
+      VI_VANUMBER: '7301123456789012',
+      VI_TRACENO: '000001',
+      VI_TRNDATE: '2021-09-13T14:00:00+07:00',
+      VI_DELCHANNEL: '099',
+    },
+  }
+
   try {
+    const customerName = 'JOKO WIDODO'
+    const billAmount = '10000.00'
+
     res.send({
-      message: '',
+      GetBillRs: {
+        CUSTNAME: `QOPNET ${customerName}`,
+        BILL_AMOUNT: billAmount,
+        VI_CCY: '360',
+        RefInfo: [
+          { RefName: 'BillPeriod', RefValue: '02' },
+          { RefName: 'BillLateCharges', RefValue: '0' },
+        ],
+        STATUS: '00',
+      },
     })
   } catch (error) {
     res.status(500).send({
-      message: '',
-      error,
+      GetBillRs: {
+        CUSTNAME: '',
+        BILL_AMOUNT: '',
+        VI_CCY: '360',
+        RefInfo: [
+          { RefName: 'BillPeriod', RefValue: '' },
+          { RefName: 'BillLateCharges', RefValue: '' },
+        ],
+        STATUS: '14',
+      },
     })
   }
 }
