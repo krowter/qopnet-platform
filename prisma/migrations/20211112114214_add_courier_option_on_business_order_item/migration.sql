fd8d37920bcff5ff9c8582c3972a9f687cce8859
M Haidar Hanif <git@mhaidarhanif.com>
2021-11-13 02:02:57 +0700

    feat(prisma): reinit prisma because of courier vehicle

--- prisma/migrations/20211112114214_add_courier_option_on_business_order_item/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "business_order_items" ADD COLUMN     "shipmentCourierId" TEXT;
-
--- AddForeignKey
-ALTER TABLE "business_order_items" ADD CONSTRAINT "business_order_items_shipmentCourierId_fkey" FOREIGN KEY ("shipmentCourierId") REFERENCES "couriers"("id") ON DELETE SET NULL ON UPDATE CASCADE;
